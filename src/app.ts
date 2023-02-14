8861ffde44ba831fddfff1d0d41255ea1adcf30d
Marchi8 <luandeoliveirasoares@gmail.com>
2023-02-14 12:01:21 -0300

    build: initial config

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import "reflect-metadata"
+import "express-async-errors"
+import express from "express"
+import { appRoutes } from "./routes"
+import handleErrorMiddleware from "./middlewares/handleError.middleware"
+import cors from "cors"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+appRoutes(app)
+
+app.use(handleErrorMiddleware)
+
+export default app
---
+++ src/data-source.ts
@@ -0,0 +1,30 @@
+import { DataSource } from "typeorm";
+import "dotenv/config"
+// import { User } from "./entities/user.entity";
+// import { initial1676095782228 } from "./migrations/1676095782228-initial"
+
+const AppDataSource = new DataSource(
+    process.env.NODE_ENV === "test" ?
+        {
+            type: "sqlite",
+            database: ":memory:",
+            synchronize: true,
+            entities: ["src/entities/*.ts"]
+        } :
+        {
+            type: "postgres",
+            host: process.env.DB_HOST,
+            port: 5432,
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB,
+            logging: true,
+            synchronize: true,
+            entities: ['src/entities/*.ts'],
+            migrations: ['src/migrations/*.ts']
+            // entities: [User],
+            // migrations: [initial1676095782228]
+        }
+)
+
+export default AppDataSource
