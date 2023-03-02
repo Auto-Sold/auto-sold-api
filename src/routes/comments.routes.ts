27af79e5ae8254e3a72ef68bc6b146c520540b9b
Yuri Bentes <eypbentes@gmail.com>
2023-03-02 17:55:29 -0300

    fix:Mudanças na route de comments.

---
+++ src/routes/comments.routes.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import createAnnounceController from "../controllers/announce/createAnnounce.controller";
+import deleteAnnounceController from "../controllers/announce/deleteAnnounce.controller";
+import listAnnounceController from "../controllers/announce/listAnnounce.controller";
+import retrieveAnnounceController from "../controllers/announce/retrieveAnnouce.controller";
+import ensureauthToken from "../middlewares/ensureAuthToken.middleware";
+
+const routes = Router()
+
+export const commentsRoutes = () => {
+
+    routes.post("", createAnnounceController)
+    // routes.get("", ensureauthToken, listAnnounceController)
+    // routes.get("/:id", ensureauthToken, retrieveAnnounceController)
+    // routes.delete( "/:id", deleteAnnounceController )
+    // routes.patch("/:id", ensureauthToken,)
+
+    return routes
+}
